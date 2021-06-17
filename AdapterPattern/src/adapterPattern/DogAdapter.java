afaebffe9252894baea155b42fe62636962463c5
gaojun-12138 <997136782@qq.com>
2021-06-18 01:14:19 +0800

    设计模式

---
+++ AdapterPattern/src/adapterPattern/DogAdapter.java
@@ -0,0 +1,16 @@
+package adapterPattern;
+
+public class DogAdapter extends Dog implements Robot{
+    public void cry() {
+        System.out.println("机器人模仿：");
+        super.wang();
+
+    }
+
+    public void move() {
+        System.out.println("机器人模仿：");
+        super.run();
+
+    }
+
+}
